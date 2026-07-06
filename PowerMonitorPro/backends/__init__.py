5161db1ffff6bf74d827605b872be0f304f89539
jazamed <forums@theahmedfamily.co.uk>
2026-07-06 14:22:25 +0100

    Delete PowerMonitorPro directory

--- PowerMonitorPro/backends/__init__.py
+++
@@ -1,5 +0,0 @@
-from .fnirsi import FNIRSIBackend, SUPPORTED_PROFILES
-from .atorch_t3 import AtorchT3Backend
-from .rp2040_ina260 import RP2040INA260Backend
-
-from .esp32_ina260 import ESP32INA260Backend
